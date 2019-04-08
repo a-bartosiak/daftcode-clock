710b24a900ef935a6a3a9ef9300f7f0885e8bd00
Aleksandra Bogdanska <alexandra.bogdanska@gmail.com>
2019-04-08 19:15:22 +0200

    added webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style/style.css';
+import script from './script';
+
+script();
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    mode: 'development', // 'production'
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
+            {
+
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
